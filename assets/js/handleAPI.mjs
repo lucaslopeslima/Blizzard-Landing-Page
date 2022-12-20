485c2f6b3913da5a52b9f020dc76d1405b2b2733
Lucas L. Lima <lucaslopes.developer@gmail.com>
2022-12-19 23:51:55 -0300

    feat- constructor

--- assets/js/handleAPI.mjs
+++ assets/js/handleAPI.mjs
@@ -1,5 +1,9 @@
 console.log('handleAPI.js ON')
 const url = 'https://api.brchallenges.com/api/blizzard/games'
+const dropdownJogos = document.querySelector('.dropdown-jogos')
+const dropdownEsportes = document.querySelector('.dropdown-esportes')
+
+
 
 fetch(url).then((res)=>{
     return res.json()
@@ -7,7 +11,7 @@ fetch(url).then((res)=>{
     console.log(data)
     data.forEach(element => {
         console.log(element)
-        
+
     });
 }).catch(e => {
     console.log(e);
