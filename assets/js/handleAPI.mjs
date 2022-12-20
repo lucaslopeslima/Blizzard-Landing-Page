c1adfacb31577f079b69e5ed14bd06ae6a53ffa1
Lucas L. Lima <lucaslima.webdev@gmail.com>
2022-12-19 23:46:12 -0300

    feat- fetch

--- assets/js/handleAPI.mjs
+++ assets/js/handleAPI.mjs
@@ -1,1 +1,15 @@
 console.log('handleAPI.js ON')
+const url = 'https://api.brchallenges.com/api/blizzard/games'
+
+fetch(url).then((res)=>{
+    return res.json()
+}).then((data)=>{
+    console.log(data)
+    data.forEach(element => {
+        console.log(element)
+        
+    });
+}).catch(e => {
+    console.log(e);
+    return e;
+});
