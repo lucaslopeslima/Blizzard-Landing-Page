732a140eb379911056240ca0ac92dba4a07b4381
Lucas L. Lima <lucaslima.webdev@gmail.com>
2022-12-20 22:26:16 -0300

    feat- jogos exclusivos constructor

--- assets/js/handleAPI.mjs
+++ assets/js/handleAPI.mjs
@@ -2,7 +2,8 @@ console.log('handleAPI.js ON')
 const url = 'https://api.brchallenges.com/api/blizzard/games'
 const dropdownJogos = document.querySelector('.dropdown-jogos')
 const dropdownEsportes = document.querySelector('.content-esportes')
-console.log(dropdownJogos)
+const jogosExclusivos = document.querySelector('.main-content')
+console.log(jogosExclusivos)
 
 
 fetch(url).then((res)=>{
@@ -13,6 +14,7 @@ fetch(url).then((res)=>{
         //console.log(element)
         //console.log(element.logo)
         criarDropdownJogos(element.logo, element.name)
+        criarJogosExclusivos(element.image, element.name, element.category)
     });
 }).catch(e => {
     console.log(e);
@@ -31,4 +33,16 @@ function criarDropdownJogos(logo, titulo){
             <p>${titulo}</p>
         </div>
     </div>`;
+}
+
+function criarJogosExclusivos(imagem, titulo, categoria){
+
+    jogosExclusivos.innerHTML +=
+    `<div class="jogos-item">
+        <img src="${imagem}" alt="${imagem}">
+        <div class="jogos-text">
+            <h4>${titulo}</h4>
+            <p>${categoria}</p>
+        </div>
+    </div>`
 }
