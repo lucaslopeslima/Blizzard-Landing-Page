624e0520aa79b9494b519072ed8db9f00192dd39
Lucas L. Lima <lucaslima.webdev@gmail.com>
2022-12-21 23:44:41 -0300

    feat- Sistem detection ON

--- assets/js/main.mjs
+++ assets/js/main.mjs
@@ -1,3 +1,7 @@
 console.log('main.js ON')
 const footerBtn = document.querySelector('.footer-btn')
 console.log(footerBtn)
+console.log(window.navigator.userAgent)
+if (window.navigator.userAgent.indexOf("Windows")!= -1) footerBtn.innerHTML = `<i class="fa-brands fa-windows"></i><span>Baixar para Windows</span>`;
+if (window.navigator.userAgent.indexOf("Linux")!= -1) footerBtn.innerHTML = `<i class="fa-brands fa-linux"></i><span>Baixar para Linux</span>`;
+if (window.navigator.userAgent.indexOf("Mac")!= -1) footerBtn.innerHTML = `<i class="fa-brands fa-apple"></i><span>Baixar para Mac OS</span>`;
